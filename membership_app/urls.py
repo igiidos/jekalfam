1463f7889bf67b21d45de624eb602808efdbcfc8
igiidos <igiidos@gmail.com>
2018-12-14 15:47:38 +0900

    urls / 이거 수정

--- membership_app/urls.py
+++ membership_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.monthly_list, name='monthly_list'),
-	path('/add_month', views.add_month, name='add_month'),
-	path('/month_detail/<pk>', views.month_detail, name='month_detail')
+	path('add_month/', views.add_month, name='add_month'),
+	path('month_detail/<pk>/', views.month_detail, name='month_detail')
 ]
