3acd32b36f2b478cf7d234485a531007329df365
igiidos <igiidos@gmail.com>
2021-08-04 00:26:42 +0900

    서버이동중

--- jekal_membership_project/settings.py
+++ jekal_membership_project/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'jekal_membership_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['JEKALFAM_DB_NAME'],
+        'USER': os.environ['JEKALFAM_DB_USER'],
+        'PASSWORD': os.environ['JEKALFAM_DB_PASSWORD'],
+        'HOST': os.environ['JEKALFAM_DB_HOST'],
+        'PORT': os.environ['JEKALFAM_DB_PORT']
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
